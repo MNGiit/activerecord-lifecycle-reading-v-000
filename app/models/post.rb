178844a24f8ee99817601a55ef30dc7c46eb83e7
Mohammed Noori <kmanoorib@gmail.com>
2019-07-04 03:02:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,8 @@ class Post < ActiveRecord::Base
   validate :is_title_case 
   
   # New Code!
-  before_save :make_title_case
+  # before_save :make_title_case
+  before_validation :make_title_case
   # End of New Code!
   private
 
